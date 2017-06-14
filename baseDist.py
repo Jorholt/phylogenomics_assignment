fcb0a39f28598b2507bbc54e3051fa99a4625c57
John Jorholt <john.jorholt@ki.se>
2017-06-14 14:11:21 +0200

    Added countBases function to baseDist.py

--- baseDist.py
+++ baseDist.py
@@ -1,1 +1,26 @@
 
+def countBases(sequence):
+    assert type(sequence) is str
+    a = 0
+    c = 0
+    g = 0
+    t = 0
+    for i in sequence:
+        assert i in 'ACGT'
+        if i == 'A':
+            a+= 1
+        elif i == 'C':
+            c += 1
+        elif i == 'G':
+            g += 1
+        elif i == 'T':
+            t += 1
+    baselist = []
+    baselist.append(a/len(sequence))
+    baselist.append(c/len(sequence))
+    baselist.append(g/len(sequence))
+    baselist.append(t/len(sequence))
+
+    assert abs(1 - sum(baselist)) < 0.001
+
+    return baselist

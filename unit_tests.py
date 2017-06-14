a0c2969a9b5a65ef11949fb074fd1e0e3aedd9ff
lekdahl <ludvig.ekdahl@gmail.com>
2017-06-14 14:26:26 +0200

    fixed unit_test.py

--- unit_tests.py
+++ unit_tests.py
@@ -1,10 +1,9 @@
-import baseDist.py *
+from baseDist import *
 
 def test1():
-	testArray1 = [0.25, 0.25, 0.25, 0.25]
-  testArray2 = [0.2, 0.2, 0.3, 0.3]
+	testArray1	=	[0.25, 0.25, 0.25, 0.25]
+	testArray2	=	[0.2, 0.2, 0.3, 0.3]
 	obs = calculateDist(testArray1, testArray2)
 	exp = 0.05
 	assert obs == exp
-	
-	
+
